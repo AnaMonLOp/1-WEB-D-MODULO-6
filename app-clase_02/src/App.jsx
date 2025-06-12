f2834556b747ae245d0581ef16de95e5f5cb8ceb
Ana Monroy <anamonroyl2112@gmail.com>
2025-06-11 18:42:22 -0600

    Clase 2: Concepto Hook

---
+++ app-clase_02/src/App.jsx
@@ -0,0 +1,12 @@
+import Perfil from './components/Perfil'
+
+function App() {
+
+  return (
+    <>
+      <Perfil nombre={"Ana Monroy"} rol={"Software Engineer"} />
+    </>
+  )
+}
+
+export default App
